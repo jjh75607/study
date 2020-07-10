ab2917658ae97854f825b68cc6dde34c4e43e25b
jjh75607 <dkrak5738@naver.com>
2020-07-10 23:52:30 +0900

    js bdd

--- javascript/modernJs/001.js
+++ javascript/modernJs/001.js
@@ -39,3 +39,12 @@ Boolean(value)
 
 x = (a !== null && a !== undefined) ? a : b;
 x = a ?? b;
+
+let sum = (a, b) => a + b;
+
+/* 위 화살표 함수는 아래 함수의 축약 버전입니다.
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
