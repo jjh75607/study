c4de5fe6563a1896825b658715360923e5b7853d
jjh75607 <dkrak5738@naver.com>
2020-11-10 15:27:08 +0900

    backjun

--- 백준/백준/15596.c
+++ 백준/백준/15596.c
@@ -4,23 +4,23 @@ long long sum(int *a, int n);
 
 int main() {
     int n;
-    
+
     scanf("%d", &n);
     int arr[n];
-    
+
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    
+
     sum(arr, n);
 }
 
 long long sum(int *a, int n) {
     long long ans = 0;
-    
+
     for (int i = 0; i < n; i++) {
         ans += a[i];
     }
-    
+
     return ans;
 }
